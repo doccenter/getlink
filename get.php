4ba10102a2f10e852f1316390297924e921e8979
doccenter <nguyentanhau165997@gmail.com>
2018-12-10 04:15:48 -0800

    a

--- get.php
+++ get.php
@@ -1,4 +1,5 @@
 <?php
 $id = $_GET['id'];
 $details = json_decode(file_get_contents("http://demo.filedeo.stream/drive/json.php?url=https://drive.google.com/file/d/1U-dBNjsDcAoHL-FFlMpO5ON3NMmUirlj/view"));
-var_dump($details);
+
+echo '{"id":"' . $details->id . '","title":"' . $details->title . '","image":"' . $details->image . '","label":"' . $details->label . '","file":"' . $details->file . '","type":"' . $details->type . '","embed_id":"' . $details->embed_id . '"}';
--- index.php
+++ index.php
@@ -11,11 +11,18 @@
     <script>
 
         $(document).ready(function () {
-            $('#ok').on('click',function () {
+            $('#ok').on('click', function () {
                 $.ajax({
-                    url:'get.php?id='+$('#id').val(),
-                    success:function (data) {
-                        console.log(data);
+                    url: 'get.php?id=' + $('#id').val(),
+                    success: function (data) {
+                        var o = JSON.parse(data);
+                        $('#txtID').val(o.id);
+                        $('#txtTitle').val(o.title);
+                        $('#txtImage').val(o.image);
+                        $('#txtLabel').val(o.label);
+                        $('#txtFile').val(o.file);
+                        $('#txtType').val(o.type);
+                        $('#txtEmbed_ID').val(o.embed_id);
                     }
                 })
             });
@@ -26,8 +33,135 @@
 </head>
 <body>
 
-    <input placeholder="Id of link google drive sharable" name="id" type="text"><br>
-    <input type="button" id="ok" value="Get">
+<div class="container" style="padding-top: 30px">
+    <h1>Hệ thống get link google drive</h1>
+    <div class="col-sm-12">
+        <input type="text" id="id" placeholder="Nhập id link google drive có khả năng chia sẻ cho tất cả mọi người"
+               class="form-control"><br>
+        <button id="ok" class="btn btn-primary">Lấy thông tin</button>
+    </div>
+    <div class="col-sm-12">
+        <h3>Thông tin bên dưới</h3>
+        ID:
+        <div class="input-group">
+            <input class="form-control" id="txtID" readonly/><span class="input-group-addon btn btn-primary" id="btnID">Copy To Clipboard</span>
+        </div>
+        <br>
+        Title:
+        <div class="input-group">
+            <input class="form-control" id="txtTitle" readonly/><span class="input-group-addon btn btn-primary"
+                                                                      id="btnTitle">Copy To Clipboard</span>
+        </div>
+        <br>
+        Image:
+        <div class="input-group">
+            <input class="form-control" id="txtImage" readonly/><span class="input-group-addon btn btn-primary"
+                                                                      id="btnImage">Copy To Clipboard</span>
+        </div>
+        <br>
+        Label:
+        <div class="input-group">
+            <input class="form-control" id="txtLabel" readonly/><span class="input-group-addon btn btn-primary"
+                                                                      id="btnLabel">Copy To Clipboard</span>
+        </div>
+        <br>
+        File:
+        <div class="input-group">
+            <input class="form-control" id="txtFile" readonly/><span class="input-group-addon btn btn-primary"
+                                                                     id="btnFile">Copy To Clipboard</span>
+        </div>
+        <br>
+        Type:
+        <div class="input-group">
+            <input class="form-control" id="txtType" readonly/><span class="input-group-addon btn btn-primary"
+                                                                     id="btnType">Copy To Clipboard</span>
+        </div>
+        <br>
+        <div class="input-group">
+            Embed_ID: <input class="form-control" id="txtEmbed_ID" readonly/><span
+                    class="input-group-addon btn btn-primary" id="btnEmbed_ID">Copy To Clipboard</button></span>
+        </div>
+    </div>
+</div>
+<script>
+    function copyText(text) {
+        function selectElementText(element) {
+            if (document.selection) {
+                var range = document.body.createTextRange();
+                range.moveToElementText(element);
+                range.select();
+            } else if (window.getSelection) {
+                var range = document.createRange();
+                range.selectNode(element);
+                window.getSelection().removeAllRanges();
+                window.getSelection().addRange(range);
+            }
+        }
 
+        var element = document.createElement('DIV');
+        element.textContent = text;
+        document.body.appendChild(element);
+        selectElementText(element);
+        document.execCommand('copy');
+        element.remove();
+    }
+
+
+    var txtID = document.getElementById('txtID');
+    var btnID = document.getElementById('btnID');
+
+    var txtTitle = document.getElementById('txtTitle');
+    var btnTile = document.getElementById('btnTitle');
+
+    var txtImage = document.getElementById('txtImage');
+    var btnImage = document.getElementById('btnImage');
+
+    var txtLabel = document.getElementById('txtLabel');
+    var btnLabel = document.getElementById('btnLabel');
+
+    var txtFile = document.getElementById('txtFile');
+    var btnFile = document.getElementById('btnFile');
+
+    var txtType = document.getElementById('txtType');
+    var btnType = document.getElementById('btnType');
+
+    var txtEmbed_ID = document.getElementById('txtEmbed_ID');
+    var btnEmbed_ID = document.getElementById('btnEmbed_ID');
+
+    btnID.addEventListener('click', function () {
+        copyText(txtID.value);
+        btnID.innerHTML = 'Copied !';
+    });
+    btnTile.addEventListener('click', function () {
+        copyText(txtTitle.value);
+        btnTile.innerHTML = 'Copied !';
+    });
+
+    btnImage.addEventListener('click', function () {
+        copyText(txtImage.value);
+        btnImage.innerHTML = 'Copied !';
+    });
+
+    btnLabel.addEventListener('click', function () {
+        copyText(txtLabel.value);
+        btnLabel.innerHTML = 'Copied !';
+    });
+
+    btnFile.addEventListener('click', function () {
+        copyText(txtFile.value);
+        btnFile.innerHTML = 'Copied !';
+    });
+
+    btnType.addEventListener('click', function () {
+        copyText(txtType.value);
+        btnType.innerHTML = 'Copied !';
+    });
+
+    btnEmbed_ID.addEventListener('click', function () {
+        copyText(txtEmbed_ID.value);
+        btnEmbed_ID.innerHTML = 'Copied !';
+    });
+
+</script>
 </body>
 </html>
