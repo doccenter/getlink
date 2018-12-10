a6d773b839446f78e05e6bddd3544845681d2c1f
doccenter <nguyentanhau165997@gmail.com>
2018-12-10 04:25:33 -0800

    a

--- get.php
+++ get.php
@@ -1,5 +1,5 @@
 <?php
 $id = $_GET['id'];
-$details = json_decode(file_get_contents("http://demo.filedeo.stream/drive/json.php?url=https://drive.google.com/file/d/1U-dBNjsDcAoHL-FFlMpO5ON3NMmUirlj/view"));
+$details = json_decode(file_get_contents("http://demo.filedeo.stream/drive/json.php?url=https://drive.google.com/file/d/".$id."/view"));
 
 echo '{"id":"' . $details->id . '","title":"' . $details->title . '","image":"' . $details->image . '","label":"' . $details->label . '","file":"' . $details->file . '","type":"' . $details->type . '","embed_id":"' . $details->embed_id . '"}';
