a97485f1df140958f731a189f90c3c5d2f176013
doccenter <nguyentanhau165997@gmail.com>
2018-12-13 00:50:39 +0700

    kk

--- rest.php
+++ rest.php
@@ -1,4 +1,4 @@
 <?php
 $id = $_GET['id'];
-$details = json_decode(file_get_contents("http://demo.filedeo.stream/drive/json.php?url=https://drive.google.com/file/d/" . $id . "/view"));
+$details = json_decode(file_get_contents("http://demo.filedeo.stream/drive/json.php?url=".$id));
 echo $details->file;
